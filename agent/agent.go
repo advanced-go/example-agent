8145e51e4a4efda5357d264993d0cd330e31b843
go-ai-agent <mark.wilsonai@outlook.com>
2023-11-10 13:55:26 -0600

    Adding functionality

--- agent/agent.go
+++ agent/agent.go
@@ -3,8 +3,11 @@ package agent
 import (
 	"fmt"
 	"github.com/go-ai-agent/core/runtime"
+	"github.com/go-ai-agent/example-domain/activity"
 	"github.com/go-ai-agent/example-domain/slo"
 	"github.com/go-ai-agent/example-domain/timeseries"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -27,10 +30,11 @@ func run(interval time.Duration, quit <-chan struct{}) {
 			if !status.OK() {
 				fmt.Printf("error reading active SLO -> %v\n", status)
 			} else {
-				ms := durationMS(slo)
+				ms := durationMS(slo.Threshold)
 				for _, e := range ts {
 					if e.Duration > ms {
-						addActivity()
+						desc := fmt.Sprintf("duration [%v] is over threshold [%v]", e.Duration, ms)
+						addActivity("trace", "agent-name", slo.Controller, desc)
 					}
 				}
 			}
@@ -53,11 +57,79 @@ func activeSLO() (slo.EntryV1, *runtime.Status) {
 	return entries[len(entries)-1], runtime.NewStatusOK()
 }
 
-func durationMS(s slo.EntryV1) int {
-	return 0
+func durationMS(threshold string) int {
+	if len(threshold) == 0 {
+		return -1
+	}
+	s := strings.Split(threshold, "/")
+	if len(s) < 2 {
+		return -1
+	}
+	dur, err := ParseDuration(s[1])
+	if err != nil {
+		return -1
+	}
+	return int(dur / time.Millisecond)
 }
 
-func addActivity() {
-	//act := activity.EntryV1{}
-	//
+func addActivity(act, agent, controller, desc string) *runtime.Status {
+	e := activity.EntryV1{ActivityType: act, Agent: agent, Controller: controller, Description: desc}
+	var entries []activity.EntryV1
+
+	entries = append(entries, e)
+	_, status := activity.Do[[]activity.EntryV1](nil, "PUT", "", "", entries)
+	return status
+}
+
+func ParseDuration(s string) (time.Duration, error) {
+	if s == "" {
+		return 0, nil
+	}
+	tokens := strings.Split(s, "ms")
+	if len(tokens) == 2 {
+		val, err := strconv.Atoi(tokens[0])
+		if err != nil {
+			return 0, err
+		}
+		return time.Duration(val) * time.Millisecond, nil
+	}
+	tokens = strings.Split(s, "µs")
+	if len(tokens) == 2 {
+		val, err := strconv.Atoi(tokens[0])
+		if err != nil {
+			return 0, err
+		}
+		return time.Duration(val) * time.Microsecond, nil
+	}
+	tokens = strings.Split(s, "m")
+	if len(tokens) == 2 {
+		val, err := strconv.Atoi(tokens[0])
+		if err != nil {
+			return 0, err
+		}
+		return time.Duration(val) * time.Minute, nil
+	}
+	// Assume seconds
+	tokens = strings.Split(s, "s")
+	if len(tokens) == 2 {
+		s = tokens[0]
+	}
+	val, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, err
+	}
+	return time.Duration(val) * time.Second, nil
+}
+
+func Example_Analyze(ts []timeseries.EntryV1) {
+	/*
+		ms := durationMS(slo.Threshold)
+		for _, e := range ts {
+			if e.Duration > ms {
+				desc := fmt.Sprintf("duration [%v] is over threshold [%v]", e.Duration, ms)
+				addActivity("trace", "agent-name", slo.Controller, desc)
+			}
+		}
+
+	*/
 }
--- agent/agent_test.go
+++ agent/agent_test.go
@@ -1,1 +1,14 @@
 package agent
+
+import "fmt"
+
+func Example_durationMS() {
+	s := "99.9/800ms"
+
+	d := durationMS(s)
+	fmt.Printf("test: durationMS -> %v\n", d)
+
+	//Output:
+	//test: durationMS -> 800
+
+}
